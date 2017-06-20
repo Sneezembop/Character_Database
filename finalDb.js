7f1d5e7d68f73253c322571be3e0570389adde33
MBenjamin <benjamin.mi@husky.neu.edu>
2017-06-20 15:34:38 -0400

    Updating main menu

--- finalDb.js
+++ finalDb.js
@@ -36,8 +36,8 @@ function quit() {
 function main() {
     connection.connect();
     prompt.start();
-    
-   // load('Axiom Verge Soundtrack - Trace Awakens.mp3').then(play);
+
+    // load('Axiom Verge Soundtrack - Trace Awakens.mp3').then(play);
 
     console.log("WELCOME TO THE RPG CHARACTER DATABASE SYSTEM");
     console.log("                                 .       .                              ");
@@ -68,45 +68,84 @@ function getUserInput(params, callback) {
     });
 
 }
-var ValidCommands = ['CREATE', 'DELETE', 'UPDATE', 'VIEW', 'QUIT'];
+
 
 function processLogin(input) {
     connection.query('SELECT * FROM players WHERE player_email LIKE \'' + input.userEmail + '\';', function (error, results, fields) {
         if (error) throw error;
 
-        console.log("results:", results);
-        if (results[0] == undefined){
-            console.log("PLAYER NOT FOUND, PLEASE ENTER THE FOLLOWING INFO TO CREATE ACCOUNT");
-            getUserInput(['email', 'firstName', 'lastName'], processCreatePlayer);
+        //console.log("results:", results);
+        if (results[0] == undefined) {
+            console.log("ACCOUNT NOT FOUND, WOULD YOU LIKE TO CREATE A NEW ACCOUNT?[Y/N]");
+            getUserInput(['YorN'], processCreatePlayerQuestion);
         } else {
             playerID = results[0].player_id;
-            console.log("WELCOME "+ results[0].player_fname + "!");
-            quit();
+            console.log("WELCOME " + results[0].player_fname + "! PLEASE ENTER A COMMAND.");
+            getUserInput(["Command", "Value"], processMainMenu);
         }
     });
 }
 
-function processCreatePlayer(input){
+function processCreatePlayerQuestion(input) {
+    if (input.YorN == 'Y') {
+        console.log("PLEASE ENTER THE FOLLOWING INFO:");
+        getUserInput(['email', 'firstName', 'lastName', 'password'], processCreatePlayer);
+    } else {
+        console.log("PLEASE LOG IN:");
+        getUserInput(['userEmail', 'password'], processLogin);
+    }
+}
+
+function processCreatePlayer(input) {
     connection.query('CALL create_player(\'' + input.email + '\', \'' + input.firstName + '\', \'' + input.lastName + '\')', function (error, results, fields) {
         if (error) throw error;
-        console.log ("PLAYER CREATED, PLEASE LOG IN");
+        console.log("PLAYER CREATED, PLEASE LOG IN");
         getUserInput(['userEmail', 'password'], processLogin);
     });
 }
 
-function processUserInput(input) {
 
+var ValidMainCommands = ['CREATE', 'DELETE', 'UPDATE', 'VIEW', 'QUIT', 'HELP'];
 
-    if (ValidCommands.includes(input.command)) {
-        console.log('VALID COMMAND! ' + input.value + '\n');
-    } else {
-        console.log('INVALID COMMAND\n');
+function processMainMenu(input) {
+    switch (input.Command) {
+        case 'CREATE':
+            quit();
+            break;
+        case 'DELETE':
+            quit();
+            break;
+        case 'UPDATE':
+            quit();
+            break;
+        case 'VIEW':
+            quit();
+            break;
+        case 'QUIT':
+            quit();
+            break;
+        case 'HELP':
+            displayHelpMenu();
+            getUserInput(["Command", "Value"], processMainMenu);
+            break;
+        default:
+            console.log("INVALID COMMAND, TRY AGAIN.  TYPE HELP IF YOU NEED ASSISTANCE.");
+            console.log("PLEASE ENTER A COMMAND:");
+            getUserInput(["Command", "Value"], processMainMenu);
     }
 
-    if (input.command == 'QUIT') {
-        quit();
-    } else {
-        getUserInput(processUserInput);
-    }
 
 }
+
+function displayHelpMenu(){
+
+            console.log("HELP LIST:");
+            console.log("\t-\t-\t-\tMAIN MENU COMMANDS\t-\t-\t-");
+            console.log("COMMAND\t-\t-VALUE");
+            console.log("CREATE\t-\t[character name]\t-\tcreates a new character with the inputed name. (NOT FINISHED)");
+            console.log("VIEW\t-\t[character name]\t-\tshows the stats of the character. (NOT FINISHED)");
+            console.log("UPDATE\t-\t[character name]\t-\tallows players to update their characters. (NOT FINISHED)");
+            console.log("DELETE\t-\t[character name]\t-\tdeletes character from the system. (NOT FINISHED)");
+            console.log("HELP\t-\t[none]\t-\tshows the help menu.");
+            console.log("QUIT\t-\t[none]\t-\texits the program.");
+};
