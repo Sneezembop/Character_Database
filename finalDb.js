7ad60eb3cde0a706e0c7646c95b4e8c7e849883e
MBenjamin <benjamin.mi@husky.neu.edu>
2017-06-20 16:48:38 -0400

    Create Character hooked up.

--- finalDb.js
+++ finalDb.js
@@ -17,6 +17,8 @@ var prompt = require('prompt');
 
 // GLOBALS
 var playerID = 0;
+var playerEmail = "";
+var playerAdmin = false;
 
 main();
 
@@ -69,6 +71,8 @@ function getUserInput(params, callback) {
 
 }
 
+var mainMenuParams = ["Command"];
+var inputParams = ["value"];
 
 function processLogin(input) {
     connection.query('SELECT * FROM players WHERE player_email LIKE \'' + input.userEmail + '\';', function (error, results, fields) {
@@ -80,8 +84,9 @@ function processLogin(input) {
             getUserInput(['YorN'], processCreatePlayerQuestion);
         } else {
             playerID = results[0].player_id;
+            playerEmail = results[0].player_email;
             console.log("WELCOME " + results[0].player_fname + "! PLEASE ENTER A COMMAND.");
-            getUserInput(["Command", "Value"], processMainMenu);
+            getUserInput(mainMenuParams, processMainMenu);
         }
     });
 }
@@ -97,11 +102,22 @@ function processCreatePlayerQuestion(input) {
 }
 
 function processCreatePlayer(input) {
+
+
     connection.query('CALL create_player(\'' + input.email + '\', \'' + input.firstName + '\', \'' + input.lastName + '\')', function (error, results, fields) {
         if (error) throw error;
         console.log("PLAYER CREATED, PLEASE LOG IN");
         getUserInput(['userEmail', 'password'], processLogin);
     });
+}
+
+function processCreateCharacter(input){
+    connection.query('CALL create_character(\'' + input.characterName + '\', \'' + input.characterClass + '\', \'' + playerEmail + '\')', function (error, results, fields) {
+        if (error) throw error;
+        console.log("CHARACTER CREATED!");
+        getUserInput(mainMenuParams, processMainMenu);
+    });
+
 }
 
 
@@ -110,7 +126,7 @@ var ValidMainCommands = ['CREATE', 'DELETE', 'UPDATE', 'VIEW', 'QUIT', 'HELP'];
 function processMainMenu(input) {
     switch (input.Command) {
         case 'CREATE':
-            quit();
+            getUserInput(['characterName','characterClass'],processCreateCharacter);
             break;
         case 'DELETE':
             quit();
@@ -126,12 +142,12 @@ function processMainMenu(input) {
             break;
         case 'HELP':
             displayHelpMenu();
-            getUserInput(["Command", "Value"], processMainMenu);
+            getUserInput(mainMenuParams, processMainMenu);
             break;
         default:
             console.log("INVALID COMMAND, TRY AGAIN.  TYPE HELP IF YOU NEED ASSISTANCE.");
             console.log("PLEASE ENTER A COMMAND:");
-            getUserInput(["Command", "Value"], processMainMenu);
+            getUserInput(mainMenuParams, processMainMenu);
     }
 
 
