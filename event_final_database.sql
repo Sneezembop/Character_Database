65fb7e89fdf7f844b58dc43cd619cf1f994abf7a
Nicholas Morgan <morgan.n@husky.neu.edu>
2017-06-21 00:45:40 -0400

    Added update_healt

--- event_final_database.sql
+++ event_final_database.sql
@@ -11,7 +11,7 @@ CREATE EVENT increase_health_event
 ON SCHEDULE EVERY 30 SECOND
 DO BEGIN
     UPDATE health_points
-    SET health_points = health_points + 1;
+    SET current_health_points = current_health_points + 1;
 END//
 
 DELIMITER ;
@@ -24,6 +24,34 @@ CREATE TRIGGER health_point_update
 BEGIN
 	IF NEW.current_health_points > NEW.total_health_points THEN
 		SET NEW.current_health_points = NEW.total_health_points;
+	END IF;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS update_health;
+
+DELIMITER //
+CREATE PROCEDURE update_health
+(
+	char_name VARCHAR(64),
+    down_up ENUM('down','up'),
+    amount INT
+)
+BEGIN
+	DECLARE char_id INT;
+    
+    SELECT character_id into char_id
+    FROM characters
+    WHERE char_name = characters.character_name;
+    
+    IF down_up = 'up' THEN
+		UPDATE health_points
+		SET current_health_points = current_health_points + amount
+		WHERE char_id = character_id;
+	ELSE
+		UPDATE health_points
+		SET current_health_points = current_health_points - amount
+		WHERE char_id = character_id;
 	END IF;
 END //
 DELIMITER ;
